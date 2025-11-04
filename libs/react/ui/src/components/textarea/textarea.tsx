6e3eb06cfe1a6c401359d26f766eefc60e72200d
Kyle Nguyen <kyle.nguyen304@gmail.com>
2025-11-04 12:21:28 +0700

    fix: revert original textarea

--- libs/react/ui/src/components/textarea/textarea.tsx
+++ libs/react/ui/src/components/textarea/textarea.tsx
@@ -1,7 +1,6 @@
 import {cva, type VariantProps} from 'class-variance-authority';
 import type {ComponentProps} from 'react';
 import {cn} from 'utils/cn';
-import {Icon} from '../icon/icon';
 
 export const textareaVariants = cva('', {
   variants: {
@@ -25,24 +24,19 @@ type TextareaProps = Omit<ComponentProps<'textarea'>, 'size'> &
 
 export function Textarea({className, variant, size, ...props}: TextareaProps) {
   return (
-    <div className="relative">
-      <textarea
-        data-slot="textarea"
-        className={cn(
-          'textarea-resize-custom placeholder:text-foreground-neutral-muted w-full min-w-0 rounded-6 px-8 pr-24 text-sm leading-20 text-foreground-neutral-base shadow-border-base transition-[color,box-shadow] outline-none',
-          'hover:bg-background-field-hover',
-          'selection:bg-background-accent-neutral-soft selection:text-foreground-neutral-on-inverted',
-          'disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-background-neutral-disabled disabled:shadow-none disabled:text-foreground-neutral-disabled',
-          'focus-visible:shadow-border-interactive-with-active',
-          'aria-invalid:shadow-border-error',
-          textareaVariants({variant, size}),
-          className,
-        )}
-        {...props}
-      />
-      <div className="absolute bottom-6 right-6 w-12 h-12 pointer-events-none flex items-end justify-end text-foreground-neutral-muted z-10">
-        <Icon name="resize" />
-      </div>
-    </div>
+    <textarea
+      data-slot="textarea"
+      className={cn(
+        'textarea-resize-custom placeholder:text-foreground-neutral-muted w-full min-w-0 rounded-6 px-8 pr-24 text-sm leading-20 text-foreground-neutral-base shadow-border-base transition-[color,box-shadow] outline-none',
+        'hover:bg-background-field-hover',
+        'selection:bg-background-accent-neutral-soft selection:text-foreground-neutral-on-inverted',
+        'disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-background-neutral-disabled disabled:shadow-none disabled:text-foreground-neutral-disabled',
+        'focus-visible:shadow-border-interactive-with-active',
+        'aria-invalid:shadow-border-error',
+        textareaVariants({variant, size}),
+        className,
+      )}
+      {...props}
+    />
   );
 }
