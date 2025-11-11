4627f5089449d68d101b1a2c6c021be91389b15c
Kyle Nguyen <kyle.nguyen304@gmail.com>
2025-11-11 18:04:44 +0700

    feat(storybook): enhance Storybook configuration by adding React and Tailwind CSS plugins

--- libs/react/ui/.storybook/main.ts
+++ libs/react/ui/.storybook/main.ts
@@ -1,17 +1,19 @@
-import type {StorybookConfigVite} from '@storybook/builder-vite';
 import type {StorybookConfig} from '@storybook/react-vite';
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
 
-const config: StorybookConfig & StorybookConfigVite = {
+const config: StorybookConfig = {
   framework: '@storybook/react-vite',
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['storybook-addon-pseudo-states'],
-  core: {
-    builder: {
-      name: '@storybook/builder-vite',
-      options: {
-        viteConfigPath: 'vitest.config.ts',
-      },
-    },
+  viteFinal: (config) => {
+    // Ensure plugins array exists
+    config.plugins = config.plugins || [];
+
+    // Add React and Tailwind CSS plugins
+    config.plugins.push(react(), tailwindcss());
+
+    return config;
   },
 };
 
