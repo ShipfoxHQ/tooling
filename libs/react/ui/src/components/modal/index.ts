3a95f262b143ea14a4dbb7f476ddc50a14635a55
Kyle Nguyen <kyle.nguyen304@gmail.com>
2025-11-15 10:17:07 +0700

    feat(ui): add Modal component (#128)
    * feat(ui): add Modal component
    
    * refactor(ui): update CodeBlockFooter and Modal components to use 'div' for props and enhance ModalHeader with Text component
    
    * feat(ui): add 'bookOpen' icon to Icon component
    
    * chore(changeset): add Modal components
    
    * feat(ui): enhance Modal component stories and add viewport support
    
    * chore(changeset): add changeset for Argos CI screenshot uploads
    
    ---------
    
    Co-authored-by: Noe Charmet <noe.charmet@shipfox.io>

---
+++ libs/react/ui/src/components/modal/index.ts
@@ -0,0 +1,23 @@
+export type {
+  ModalContentProps,
+  ModalDescriptionProps,
+  ModalHeaderProps,
+  ModalOverlayProps,
+  ModalTitleProps,
+} from './modal';
+export {
+  Modal,
+  ModalBody,
+  ModalClose,
+  ModalContent,
+  ModalDescription,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  ModalPortal,
+  ModalTitle,
+  ModalTrigger,
+  modalContentVariants,
+  modalDefaultTransition,
+  modalOverlayVariants,
+} from './modal';
