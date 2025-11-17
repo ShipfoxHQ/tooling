a9c600c3f22c1b7ed20f48e15232ae3dd23ae7af
Kyle Nguyen <kyle.nguyen304@gmail.com>
2025-11-17 21:33:20 +0700

    fix(storybook): update Storybook configuration to use dynamic imports for plugins

--- libs/react/ui/.storybook/main.ts
+++ libs/react/ui/.storybook/main.ts
@@ -1,16 +1,23 @@
-import {resolve} from 'node:path';
 import type {StorybookConfig} from '@storybook/react-vite';
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
 
 const config: StorybookConfig = {
   framework: '@storybook/react-vite',
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
   addons: ['storybook-addon-pseudo-states', '@storybook/addon-vitest'],
-  viteFinal: (config) => {
-    config.plugins = config.plugins ?? [];
+  viteFinal: async (config) => {
+    const [{default: react}, {default: tailwindcss}, {fileURLToPath}, {dirname, resolve}] =
+      await Promise.all([
+        import('@vitejs/plugin-react'),
+        import('@tailwindcss/vite'),
+        import('node:url'),
+        import('node:path'),
+      ]);
 
+    config.plugins = config.plugins ?? [];
     config.plugins.push(react(), tailwindcss());
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
 
     config.resolve = config.resolve ?? {};
     config.resolve.alias = {
