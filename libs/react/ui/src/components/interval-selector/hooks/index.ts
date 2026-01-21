103793bd647355c7152b30324979783f3626e440
Noe Charmet <noe.charmet@shipfox.io>
2026-01-21 11:45:14 +0100

    Type

--- libs/react/ui/src/components/interval-selector/hooks/index.ts
+++ libs/react/ui/src/components/interval-selector/hooks/index.ts
@@ -1,4 +1,3 @@
 export * from './use-interval-selector';
 export * from './use-interval-selector-input';
 export * from './use-interval-selector-navigation';
-export * from './use-interval-selector-state';
